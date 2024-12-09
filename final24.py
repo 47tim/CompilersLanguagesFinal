d135bacccf4280275ed1e82fbced2910ff14e833
GaballaGit <lgaballal@yahoo.com>
2024-12-09 13:17:27 -0800

    convert final24 to py

---
+++ final24.py
@@ -0,0 +1,22 @@
+# 1. program f2024;
+# 2. var
+# 3. a , b2a , c , bba : integer ;
+# 4. begin
+# 5. a = 3 ;
+# 6. b2a = 14 ;
+# 7. c = 5 ;
+# 8. print ( c ) ;
+# 8. bba = ( b2a + 2 * c) * a ;
+# 9. print ( “Value=” , bba ) ;
+# 10. end
+
+a: int
+b2a: int
+c: int
+bba: int
+a = 3
+b2a = 14
+c = 15
+print(c)
+bba = ( b2a + 2 * c) * a
+print ("value=", bba)
